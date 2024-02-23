8bbcf12b20beb9105e23587b7365e3b330811588
Daniel-k-dev <juyeon10120@naver.com>
2024-02-24 03:56:49 +0900

    feat: Names클래스 getNames, toStringify, splitNames, isValid, isNamesEmpty, isNameAmountIsZero, createNameList 추가

--- src/main/java/racing/Names.java
+++ src/main/java/racing/Names.java
@@ -6,22 +6,15 @@ import java.util.Objects;
 
 public class Names {
 
-    List<Name> names;
+    private List<Name> names;
 
     public Names(List<Name> names) {
         this.names = names;
     }
 
-    public Names(String[] names) {
-        this.names = createNameList(names);
-    }
-
-    private List<Name> createNameList(String[] stringNames) {
-        List<Name> names = new LinkedList<>();
-        for (String name : stringNames) {
-            names.add(new Name(name));
-        }
-        return names;
+    public Names(String names) {
+        isValid(names);
+        this.names = createNameList(splitNames(names));
     }
 
     @Override
@@ -37,4 +30,43 @@ public class Names {
         return Objects.hash(names);
     }
 
+    public List<Name> getNames() {
+        return names;
+    }
+
+    public String toStringify() {
+        List<String> nameList = new LinkedList<>();
+        for (Name name : names) {
+            nameList.add(name.getName());
+        }
+        return String.join(",", nameList);
+    }
+
+    private String[] splitNames(String names) {
+        return names.split(",");
+    }
+
+    private void isValid(String names) {
+        String [] splitNames = splitNames(names);
+
+        if (isNamesEmpty(names) || isNameAmountIsZero(splitNames)) {
+            throw new IllegalArgumentException("한 개 이상의 자동차 이름을 입력하세요.");
+        }
+    }
+
+    private boolean isNamesEmpty(String names) {
+        return names.isEmpty();
+    }
+
+    private static boolean isNameAmountIsZero(String[] names) {
+        return 0 == names.length;
+    }
+
+    private List<Name> createNameList(String[] stringNames) {
+        List<Name> names = new LinkedList<>();
+        for (String name : stringNames) {
+            names.add(new Name(name));
+        }
+        return names;
+    }
 }
--- src/test/java/racing/NamesTest.java
+++ src/test/java/racing/NamesTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 
 public class NamesTest {
@@ -17,10 +19,28 @@ public class NamesTest {
     }
 
     @Test
-    void namesTest2() {
-        Names names = new Names(new String[]{"a", "b"});
+    void namesTestByString() {
+
+        Names names = new Names("a,b");
         assertThat(names).isEqualTo(new Names(Arrays.asList(new Name("a"), new Name("b"))));
         assertThat(names).isNotEqualTo(new Names(Arrays.asList(new Name("a"), new Name("c"))));
         assertThat(names).isNotEqualTo(new Names(Arrays.asList(new Name("a"))));
+
+        assertDoesNotThrow(() -> {
+            new Names("ab");
+            new Names("ab,asdf");
+        });
+
+        assertThatThrownBy(() -> {
+            new Names("");
+            new Names(",");
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("한 개 이상의 자동차 이름을 입력하세요.");
+    }
+
+    @Test
+    void toStringify() {
+        Names names = new Names(Arrays.asList(new Name("a"), new Name("b")));
+        assertThat(names.toStringify()).isEqualTo("a,b");
     }
 }
